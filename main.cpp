b7482fbe460f223e2ee7d81c0e5a7766ced6f0e7
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-09-14 09:50:20 +0200

    integral constant

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ struct has_static_member_function<T, std::void_t<decltype(T::print())> > : std::
 };
 
 template<typename D, typename B>
-struct has_print_and_is_base_of : std::is_base_of<B, D>, has_static_member_function<D>
+struct has_print_and_is_base_of : std::integral_constant<bool,
+        std::is_base_of<B, D>::value && has_static_member_function<D>::value>
 {
 };
 
@@ -24,6 +25,14 @@ struct Base
 };
 
 struct Struct : public Base
+{
+    static void print()
+    {
+        std::puts(__PRETTY_FUNCTION__);
+    }
+};
+
+struct NotBaseOfBaseStruct
 {
     static void print()
     {
@@ -45,9 +54,13 @@ int main()
 {
     static_assert(not has_static_member_function<int>::value, "has static print member");
     static_assert(has_static_member_function<Struct>::value, "hasn't static print member");
+    static_assert(has_static_member_function<NotBaseOfBaseStruct>::value, "hasn't static print member");
     static_assert(std::is_base_of<Base, Struct>::value, "is not base of Base");
+    static_assert(not std::is_base_of<Base, NotBaseOfBaseStruct>::value, "is base of Base");
     static_assert(has_print_and_is_base_of<Struct, Base>::value,
                   "is not derived by Base and/or hasn't a static print member");
+    static_assert(not has_print_and_is_base_of<NotBaseOfBaseStruct, Base>::value,
+                  "is derived by Base and/or hasn't a static print member");
 
     lib::print<Base>();
     lib::print<Struct>();
