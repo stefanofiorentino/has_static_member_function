a2f472a82a5e3d2553adbfdbe2c88b2f7be35791
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-09-15 22:45:58 +0200

    meaningful name/message
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ struct Struct : public Base
     }
 };
 
-struct NotBaseOfBaseStruct
+struct NotBasedStruct
 {
     static void print()
     {
@@ -46,7 +46,7 @@ namespace lib
     void print()
     {
         static_assert(has_print_and_is_base_of<T, Base>::value,
-                      "T has neither a static print member os is based on Base");
+                      "T neither has a static print member or is based on Base");
         T::print();
     }
 }
@@ -55,18 +55,18 @@ int main()
 {
     static_assert(not has_static_member_function<int>::value, "has static print member");
     static_assert(has_static_member_function<Struct>::value, "hasn't static print member");
-    static_assert(has_static_member_function<NotBaseOfBaseStruct>::value, "hasn't static print member");
+    static_assert(has_static_member_function<NotBasedStruct>::value, "hasn't static print member");
     static_assert(std::is_base_of<Base, Struct>::value, "is not base of Base");
-    static_assert(not std::is_base_of<Base, NotBaseOfBaseStruct>::value, "is base of Base");
+    static_assert(not std::is_base_of<Base, NotBasedStruct>::value, "is base of Base");
     static_assert(has_print_and_is_base_of<Struct, Base>::value,
                   "is not derived by Base and/or hasn't a static print member");
-    static_assert(not has_print_and_is_base_of<NotBaseOfBaseStruct, Base>::value,
+    static_assert(not has_print_and_is_base_of<NotBasedStruct, Base>::value,
                   "is derived by Base and/or hasn't a static print member");
 
     lib::print<Base>();
     lib::print<Struct>();
 
-//    lib::print<NotBaseOfBaseStruct>(); // compilation error
+//    lib::print<NotBasedStruct>(); // compilation error
 
     return 0;
 }
